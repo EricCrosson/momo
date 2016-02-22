12b951434b3c7282b5198353e8d86dcf52976c61
Eric Crosson <esc@ericcrosson.com>
2016-02-21 21:08:33 -0600

    add notifiers

---
+++ notifiers/notifier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Written by Eric Crosson
+# 2016-02-16
+
+# TODO: document
+
+class Notifier:
+    pass
---
+++ notifiers/pushbulletnotifier.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Written by Eric Crosson
+# 2016-02-16
+
+import notifiers.notifier
+from pushbullet import Pushbullet
+
+import stump
+import subprocess
+
+# TODO: document
+
+
+class PushbulletNotifier(Notifier):
+
+
+    def __init__(self, api_key, archive='/dev/null', **kwargs):
+        self.pb = Pushbullet(api_key)
+        self.archive = archive
+
+
+    @stump.put("Executing command: '{command}'", log=logging.DEBUG)
+    def shell_output(self, command):
+        """Return stdout of specified command in shell."""
+        return subprocess.check_output(command.split()).decode('utf-8')
+
+
+    @stump.put('Notifying user of {files}')
+    def notify(self, files):
+        """Notify connected user of files."""
+        for file in files:
+            with open(file, 'rb') as pic:
+                file_data = self.pb.upload_file(pic, name(pic))
+                push = self.pb.push_file(**file_data)
+            self.archive(file)
+
+
+    @stump.put('Archiving {file}')
+    def archive(self, file):
+        """Archive file in specified archive dir."""
+        # TODO: differentiate between video files from motion (swf, any
+        # others?) and output jpgs (any others?)
+        shell_output('cp {file} {target}'.format(file=file,
+                                                 target=self.archive).split())
